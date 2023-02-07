eb2d9f8ac86eeb9dc45ac6ae6cb4a4bc55939321
Alessandrosss <sa.scarano@outlook.com>
2023-02-07 18:58:38 +0100

    app ready to be developed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import './assets/App.css'
-
-function App() {
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Paths from "./utils/paths";
+const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome on Vity</h1>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path={Paths.Home.path} element={Paths.Layout.element}>
+          <Route index element={Paths.Home.element} />
+          <Route path={Paths.Blog.path} element={Paths.Blog.element} />
+          <Route
+            path={Paths.Application.path}
+            element={Paths.Application.element}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
