c4cf4f8d4034bf2d87b176c069b18f6e21ed5f8f
Alessandrosss <sa.scarano@outlook.com>
2023-02-08 19:45:13 +0100

    navbar and landingpage styled

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./elements/Layout";
 import Paths from "./utils/paths";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={Paths.Home.path} element={Paths.Layout.element}>
+        <Route path={Paths.Home.path} element={<Layout />}>
           <Route index element={Paths.Home.element} />
           <Route path={Paths.Blog.path} element={Paths.Blog.element} />
           <Route
