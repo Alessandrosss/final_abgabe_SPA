a9a6f2a1f4c12c2322651ff821daa04de1901f0d
Alessandrosss <sa.scarano@outlook.com>
2023-02-20 18:04:24 +0100

    added favicon

--- frontend-v2/src/Navbar.jsx
+++ frontend-v2/src/Navbar.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import {
+  Link,
+  useLocation,
+} from 'react-router-dom'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
-import CssBaseline from '@mui/material/CssBaseline'
 import Divider from '@mui/material/Divider'
 import Drawer from '@mui/material/Drawer'
 import IconButton from '@mui/material/IconButton'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
-import ListItemText from '@mui/material/ListItemText'
 import MenuIcon from '@mui/icons-material/Menu'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
@@ -20,6 +21,7 @@ import Blog from './pages/Blog'
 import WebApplication from './pages/WebApplication'
 
 const drawerWidth = 240
+
 const paths = {
   Homepage: {
     title: 'Homepage',
